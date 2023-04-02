4eca6ce96027ae850d6437cf2aa5e931fdcaa2a9
Gino <georgino197@gmail.com>
2023-04-02 21:25:22 +0200

    Simple splash screen

---
+++ app/src/main/java/com/gsoft/wallet/view/activities/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.gsoft.wallet.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.gsoft.wallet.R;
+import com.gsoft.wallet.utils.Utils;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Utils utils = new Utils(this);
+        Thread splash=new Thread() {
+            public void run() {
+                try{
+                    sleep(2*1000);
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+                }catch (Exception e){
+                    utils.toast(e.getMessage());
+                }
+            }
+        };
+        splash.start();
+    }
+}
